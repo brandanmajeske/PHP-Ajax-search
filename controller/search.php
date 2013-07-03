ac05eb9f986362ad9b9a31da37f48ff0d5ec50d2
Brandan Majeske <bmajeske@me.com>
2013-07-03 12:13:20 -0600

    added view class

--- controller/search.php
+++ controller/search.php
@@ -1,2 +1,13 @@
 <?php 
-// search.php controller
+
+require_once('models/View.php');
+
+
+$view = new View();
+
+
+$view->show('header');
+$view->show('search');
+$view->show('footer');
+
+
---
+++ models/View.php
@@ -0,0 +1,17 @@
+<?php
+
+class View {
+	
+	public function show($template, $data = array(), $data2 = array()) {
+		$templatePath = "views/${template}.inc";
+		if (file_exists($templatePath)){
+			
+			$pageTitle = '';
+			$subHeading = '';
+
+			include $templatePath;
+		}
+	}
+
+
+}
